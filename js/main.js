495e499bd675a9d45dd2c77b841bcc19815c2aa9
ymiya17 <78059407+ymiya17@users.noreply.github.com>
2021-04-20 10:22:39 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+{
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
+
+  let startTime;
+  let timeoutId;
+  let elapsedTime = 0;
+
+  function countUp() {
+    const d = new Date(Date.now() - startTime + elapsedTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`;
+
+    timeoutId = setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+function setButtonStateInitial(){
+  start.disabled = false;
+  stop.disabled = true;
+  reset.disabled = true;
+}
+function setButtonStateRunning(){
+  start.disabled = true;
+  stop.disabled = false;
+  reset.disabled = true;
+}
+function setButtonStateStopped(){
+  start.disabled = false;
+  stop.disabled = true;
+  reset.disabled = false;
+}
+setButtonStateInitial();
+
+  start.addEventListener('click', () => {
+    setButtonStateRunning();
+    startTime = Date.now();
+    countUp();
+  });
+
+  stop.addEventListener('click', () => {
+    setButtonStateStopped();
+    clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime;
+  });
+
+  reset.addEventListener('click', () => {
+    setButtonStateInitial();
+    timer.textContent = '00:00.000';
+    elapsedTime = 0;
+  });
+}
